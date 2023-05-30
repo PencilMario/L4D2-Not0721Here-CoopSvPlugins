290e488cab04bc1bb459f5b1aa2700b20aafd707
PencilMario <Sir.P@foxmail.com>
2023-05-30 11:20:48 +0800

    更新脚本

---
+++ update.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "upload demos"
+
+#bash /home/steam/L4D2-Not0721Here-CoopSvPlugins/map_init.sh
+
+echo "Get Plugin updates";
+cd /home/steam/L4D2-Not0721Here-CoopSvPlugins/;
+git reset --hard
+git pull --rebase;
+git status;
+
+directories=("/home/steam/Steam/steamapps/common/l4d2versus/left4dead2" "/home/steam/Steam/steamapps/common/l4d2/left4dead2")
+
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
+
+        # 可以热更新的内容
+        # 其他文件残余内容短时间内应该不会影响 直接覆盖即可
+
+        #rm -rf "$dir/addons/sourcemod/configs/"*
+        #rm -rf "$dir/addons/sourcemod/data/"*
+        #rm -rf "$dir/addons/sourcemod/gamedata/"*
+        rm -rf "$dir/addons/sourcemod/plugins/"*
+        #rm -rf "$dir/addons/sourcemod/translations/"*
+        #rm -rf "$dir/scripts/vscripts/"*
+
+        #rm -rf "$dir/cfg/cfgogl/"*
+        #rm -rf "$dir/cfg/mixmap/"*
+        #rm -rf "$dir/cfg/sourcemod/"*
+        #rm -rf "$dir/cfg/stripper/"*
+        
+        \cp -r /home/steam/L4D2-Not0721Here-CoopSvPlugins/addons/sourcemod/configs/* "$dir/addons/sourcemod/configs/";
+        \cp -r /home/steam/L4D2-Not0721Here-CoopSvPlugins/addons/sourcemod/data/* "$dir/addons/sourcemod/data/";
+        \cp -r /home/steam/L4D2-Not0721Here-CoopSvPlugins/addons/sourcemod/gamedata/* "$dir/addons/sourcemod/gamedata/";
+        \cp -r /home/steam/L4D2-Not0721Here-CoopSvPlugins/addons/sourcemod/plugins/* "$dir/addons/sourcemod/plugins/";
+        \cp -r /home/steam/L4D2-Not0721Here-CoopSvPlugins/addons/sourcemod/translations/* "$dir/addons/sourcemod/translations/";
+        \cp -r /home/steam/L4D2-Not0721Here-CoopSvPlugins/scripts/* "$dir/scripts/";
+        \cp -r /home/steam/L4D2-Not0721Here-CoopSvPlugins/cfg/* "$dir/cfg/";
+        chmod 777 "$dir/"
+        echo "Updated | $dir"
+    else
+        echo "Unexist | $dir "
+    fi
+done
+
+echo "File Copy Success";
+
+echo "==================当前commit=================="
+git log -1
+echo "================== 运行结束 =================="
---
+++ update_full.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo "Get Plugin updates";
+cd /home/steam/L4D2-Competitive-Rework/;
+git reset --hard;
+git pull --rebase;
+git status;
+
+directories=("/home/steam/Steam/steamapps/common/l4d2coop/left4dead2")
+
+for dir in "${directories[@]}"; do
+    if [ -d "$dir" ]; then
+        find "$dir/addons/sourcemod/" \
+            ! -path "$dir/addons/sourcemod/logs*" \
+            ! -path "$dir/addons/sourcemod/configs/sourcebans/sourcebans.cfg" \
+            ! -path "$dir/addons/sourcemod/configs/databases.cfg" \
+            -type f -delete        
+
+        rm -rf "$dir/addons/metamod/"*
+        rm -rf "$dir/addons/l4dtoolz/"*
+        rm -rf "$dir/addons/stripper/"*
+        rm -rf "$dir/scripts/vscripts/"*
+        rm -rf "$dir/models/player/custom_player/"*
+        rm -rf "$dir/sound/kodua/fortnite_emotes/"*
+        # 剩下三个cfg应该不会被删除
+        find "$dir/cfg/cfgogl/" \
+            -type f -delete  
+        rm -rf "$dir/cfg/sourcemod/"*
+
+        rm -f "$dir/l4dtoolz.dll"
+        rm -f "$dir/l4dtoolz.so"
+        rm -f "$dir/l4dtoolz.vdf"
+        rm -f "$dir/metamod.vdf"
+
+        \cp -rp /home/steam/L4D2-Not0721Here-CoopSvPlugins/* "$dir/";
+        chmod 777 "$dir/"
+
+        echo "Updated | $dir"
+    else
+        echo "Unexist | $dir "
+    fi
+done
+echo "File Copy Success";
+
+echo "==================当前commit=================="
+git log -1
+echo "================== 运行结束 =================="
+
